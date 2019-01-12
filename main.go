61bd3a7865bc7c79773005133f59b831b6aa8ccd
armariya <arm.armariya@gmail.com>
2019-01-12 22:54:52 +0700

    add path

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+  "http"
+  "fmt"
+  "log"
+)
 
+func main() {
+  http.HandleFunc("/", Handler)
+  log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "hello")
 }
