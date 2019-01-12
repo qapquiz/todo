702e4bd58164d517144978a60ee1a18855078e19
armariya <arm.armariya@gmail.com>
2019-01-12 23:11:36 +0700

    add os

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module todo
+
+require github.com/line/line-bot-sdk-go v4.3.0+incompatible
--- main.go
+++ main.go
@@ -1,31 +1,47 @@
 package main
 
 import (
-  "net/http"
-  "fmt"
-  "log"
-  "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+const (
+	CHANNEL_SECRET = "5d7381ad6a95bde9fa069f3783078327"
+	CHANNEL_TOKEN  = "SaCwQG7BYVMGVMzRFVZDml2p8dBfUEZ/KRaHaNc0+2LhyM11Q4IeQZoZOKS1unZarPu2PxpIJJ2BAnFd1k8wQIzXqwtSUaCCTBoQhkHnjmptFD+M5hW7Zw7Zl3bNhOIGgjpssCW/6dj1WGvN56wfrwdB04t89/1O/w1cDnyilFU="
 )
 
 func determineListenAddress() (string, error) {
-  port := os.Getenv("PORT")
-  if port == "" {
-    return "", fmt.Errorf("$PORT not set")
-  }
-  return ":" + port, nil
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
 }
 
 func main() {
-  http.HandleFunc("/", Handler)
+	http.HandleFunc("/", Handler)
 
-  address, err := determineListenAddress()
-  if err != nil{ 
-    log.Fatal("$PORT not set")
-  }
+	address, err := determineListenAddress()
+	if err != nil {
+		log.Fatal("$PORT not set")
+	}
 
-  log.Fatal(http.ListenAndServe(address, nil))
+	log.Fatal(http.ListenAndServe(address, nil))
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "hello")
+	bot, err := linebot.New(CHANNEL_SECRET, CHANNEL_TOKEN)
+	if err != nil {
+		log.Fatal("Can't create bot")
+	}
+
+	fmt.Printf(r)
+
+	// bot.ReplyMessage("", linebot.NewTextMessage("Hello").Do(); err != nil {
+
+	// })
 }
